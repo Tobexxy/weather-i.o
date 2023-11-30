903e048763cd73cff845493c2ab80010f6370281
Tobexxy <tobiogunleye76@gmail.com>
2023-11-30 03:12:41 +0000

    upgraded version

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 const apiKey = "7c0f01e5543d7065beb49e91ec9476e9";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector('.search-box input');
+const container = document.querySelector('.container');
 const searchBtn = document.querySelector('.search-box button');
 const weatherIcon = document.querySelector('.weathericon');
-
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+const enter = document.getElementById("Enter");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city +`&appid=${apiKey}&units=imperial`);
+    const statusCode = response.status;// this code is used to get the website status code
+    // console.log(statusCode)
+
+    if(statusCode == '404'){
+        container.style.height = "400px";
+        error404.classList.add('active');
+        weatherBox.classList.remove('active');
+        weatherDetails.classList.remove('active');
+        return;
+
+    }
+
+    container.style.height = "555px";   
+    weatherBox.classList.add('active');      
+    weatherDetails.classList.add('active');   
+    error404.classList.remove('active');
+
+    
+
+
+
     var data = await response.json();
     document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + `<span>°C</span>`;
     document.querySelector(".description").innerHTML = data.weather[0].description;
     document.querySelector(".wee").innerHTML = data.wind.speed + 'km';
     document.querySelector(".hum").innerHTML = data.main.humidity + '%';
+   
 
   
     if(data.weather[0].main == "Thunderstorm"){
@@ -58,6 +84,16 @@ async function checkWeather(city){
 }
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value); //// This code checks goe the data in ajson format
-
 })
 
+
+function Alert(){
+    alert(enter.value);
+}
+enter.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        checkWeather(searchBox.value);
+        
+        
+    }
+});
